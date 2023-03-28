75acebd36f5ed4f9e75ac188976cdafd0d114dcd
Olufoba <oluwafoba@gmail.com>
2023-03-28 08:35:08 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * puts_half - prints half of a string
- * @str: input string
- * Return: no return
+ * puts_half - prints half of a string.
+ * @str: input string.
+ * Return: no return.
  */
-
 void puts_half(char *str)
 {
 	int count = 0, i;
